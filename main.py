f7720c2be99f43bd3edb9e175137f2b5710e9241
DHRUV-png <74760805+DHRUV-png@users.noreply.github.com>
2020-12-30 10:35:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+
+time.sleep(5)
+
+
+spam_text = "this is spam"
+for _ in range(20):
+    pyautogui.typewrite(spam_text)
+    pyautogui.press("enter")
+    time.sleep(1)
